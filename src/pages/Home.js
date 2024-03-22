c4f5b877ff4d2e62e762fb25563ba28cf094e559
Ray C <rayc2045@gmail.com>
2024-03-23 00:50:52 +0800

    Add pages

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,3 @@
-export default {
-  setup() {},
-  template: `
-    <h1>Home Page</h1>
-
-    <Modal open>
-      <router-link to="/test" class="r:1em overflow:hidden">
-        <img src="https://file.zotabox.com/610/701610/1667115_background_image.png?1708914391" width="500" height="500" />
-      </router-link>
-    </Modal>
-   `,
-};
+export default `
+  <h1>This is home page<h1>
+`;
