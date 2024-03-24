8b76dcff73cb532c10c6b2cffeea1c11fe4a032c
Ray C <rayc2045@gmail.com>
2024-03-24 09:45:19 +0800

    Update lorem component

--- src/components/LoremComponent.js
+++ src/components/LoremComponent.js
@@ -1,8 +1,13 @@
 class LoremComponent extends HTMLElement {
   constructor() {
     super();
-    this.outerHTML = `
-      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+    this.shadow = this.attachShadow({ mode: "closed" });
+  }
+  connectedCallback() {
+    this.shadow.innerHTML = `
+      <p>
+        <slot>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</slot>
+      </p>
     `;
   }
 }
